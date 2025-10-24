fc5a085bc5229d1f56221b6a70e754729846265c
uwongj <loln13082@gmail.com>
2025-10-24 10:12:13 +0700

    initial commit

--- app/src/main/java/com/example/plutoaccessories/ShopActivity.kt
+++ app/src/main/java/com/example/plutoaccessories/ShopActivity.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.plutoaccessories.R
 import com.example.plutoaccessories.adapters.ProductAdapter
+import com.example.plutoaccessories.models.Product
 
 class ShopActivity : AppCompatActivity() {
 
@@ -20,15 +21,15 @@ class ShopActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.recyclerViewProducts)
         recyclerView.layoutManager = GridLayoutManager(this, 2)
 
-        // Data dummy produk (sementara, nanti bisa diganti dari API / DB)
+        // Data dummy produk (sementara)
         productList = mutableListOf(
-            Product("Gelang Emas", "Perhiasan Premium", "Rp 2.500.000", R.drawable.gelang),
-            Product("Cincin Perak", "Elegan & Minimalis", "Rp 900.000", R.drawable.cincin),
-            Product("Kalung Mutiara", "Cantik & Anggun", "Rp 1.800.000", R.drawable.kalung),
-            Product("Jam Tangan Couple", "Klasik", "Rp 3.200.000", R.drawable.jam)
+            Product("Gelang Emas", "Perhiasan Premium", "Rp 2.500.000", R.drawable.gelangemas),
+            Product("Cincin Perak", "Elegan & Minimalis", "Rp 900.000", R.drawable.cincinperak),
+            Product("Kalung Mutiara", "Cantik & Anggun", "Rp 1.800.000", R.drawable.kalungmutiara),
+            Product("Jam Tangan Couple", "Klasik & Serasi", "Rp 3.200.000", R.drawable.jamtanganklasik)
         )
 
-        productAdapter = ProductAdapter(this, productList)
+        productAdapter = ProductAdapter(productList)
         recyclerView.adapter = productAdapter
     }
 }
